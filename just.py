94a06e8acfa4c8de1bb384e3a2ae3e3267f4c20b
Rick Leckrone <rick@picturenetcorp.com>
2023-04-28 16:19:05 -0700

    asdfasdf
    asdfasdf

--- just.py
+++ just.py
@@ -7,13 +7,20 @@ import json
 import os
 import platform
 import secrets
-
-
+import logging
 
 api = FastAPI()
 templates = Jinja2Templates(directory="templates")
 current_dir = os.getcwd()
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
+# Add a StreamHandler to log to console
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
 
 # GET ALL API >PY FILES
 
@@ -22,30 +29,10 @@ from api import upload
 api.include_router(upload.router)
 
 
-# CHECK PATH SYSTEM
+# MOUNT PATHS
 
-
-if platform.system() == "Darwin":
-    # macOS go to this path.
-    upload_dir = os.path.join(current_dir, "uploaded_files")
-    static_dir = os.path.join(current_dir, "static")
-    downloads_dir = os.path.join(current_dir, "downloads")
-    print("ITS A MAC!")
-else:
-    # Ubuntu (or other Linux distros) go to this path.
-    upload_dir = "uploaded_files"
-    static_dir = "static"
-    downloads_dir = "downloads"
-    print("ITS NOT A MAC!")
-
-# Mount the directories
-
-print(upload_dir)
-print()
-print(static_dir)
-print()
-print(downloads_dir)
-
+static_dir = os.path.join(current_dir, "static")
+upload_dir = os.path.join(current_dir, "uploaded_files")
 
 api.mount("/static", StaticFiles(directory=static_dir), name="static")
 api.mount("/uploaded_files", StaticFiles(directory=upload_dir), name="uploaded_files")
@@ -53,34 +40,29 @@ api.mount("/uploaded_files", StaticFiles(directory=upload_dir), name="uploaded_f
 
 # APPLICATION API
 
-
 @api.get("/")
 def form_post(request: Request):
-    
-    result = "Get Files To Upload"
-    print(result)
 
     # Generate a secure random session ID
     session_id = secrets.token_hex(16)
 
+    logging.info(f"Generated session ID: {session_id}")
+
     try:
         headers = {"Set-Cookie": f"session_id={session_id}; Max-Age=3600; Path=/"}
     except Exception as e:
-        print("Exception:", e)
+        logging.error(f"Error adding cookie: {e}")
 
-    print("Added the cookie.")
-    print(f"Session ID: {session_id}")
+    logging.info("Added the cookie.")
+    logging.info(f"Session ID: {session_id}")
 
     # Get the value of the session ID cookie
     cookie_value = request.cookies.get('session_id')
-    print(f"Cookie value: {cookie_value}")
-    print(f"Cookie exists: {cookie_value is not None}")
+    logging.info(f"Cookie value: {cookie_value}")
+    logging.info(f"Cookie exists: {cookie_value is not None}")
 
     # Return a TemplateResponse object that uses the upload.html template
-    return templates.TemplateResponse("upload.html", {"request": request, "result": result}, headers=headers)
-
-
-
+    return templates.TemplateResponse("upload.html", {"request": request}, headers=headers)
 
 
 @api.get("/result", response_class=HTMLResponse)
@@ -88,6 +70,7 @@ async def index(request: Request):
     filename = "test.jpg"
     
     # Run exiftool command to fetch all exif information
+    logging.info(f"Running exiftool command on file {filename}")
     exiftool_output = subprocess.check_output(["exiftool", "-j", f"static/{filename}"])
     
     # Convert the output to a list of dictionaries
@@ -102,7 +85,9 @@ async def index(request: Request):
     # Pass the table HTML and filename to the template
     return templates.TemplateResponse("index.html", {"request": request, "table_html": table_html, "filename": filename})
 
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(api, host="127.0.0.1", port=8000)
+
 

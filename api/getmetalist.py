03d303e2b10c951d2a6a55024593c093775180ad
Rick Leckrone <rick@picturenetcorp.com>
2023-08-13 10:38:30 -0700

    opensource
    lets go.

--- api/getmetalist.py
+++ api/getmetalist.py
@@ -1,44 +1,56 @@
 import os
 import logging
+import platform
 
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-import platform
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates/")
 
-# Set up logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
+def setup_logger():
+    logger_instance = logging.getLogger(__name__)
+    logger_instance.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
 
-logger.addHandler(handler)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger_instance.addHandler(handler)
+    return logger_instance
+
+
+logger = setup_logger()
+
+
+def get_upload_directory():
+    if platform.system() == "Darwin":
+        return os.path.join("uploaded_files")
+    return os.path.join("/home/uploaded_files/")
+
+
+def filter_image_files(directory):
+    valid_extensions = {'.jpg', '.jpeg', '.png', '.tiff', '.gif', '.bmp', '.psd', '.raw', '.dng', '.cr2', '.nef', '.sr2'}
+    return [
+        f for f in os.listdir(directory)
+        if os.path.isfile(os.path.join(directory, f)) and os.path.splitext(f)[1].lower() in valid_extensions
+    ]
+
 
 @router.post('/api/getmetalist')
 async def get_metadata(request: Request):
-
     logger.info("Request received for get_metadata endpoint")
 
-    if platform.system() == "Darwin":
-        # macOS go to this path.
-        upload_dir = os.path.join("uploaded_files")
-    else:
-        # Ubuntu (or other Linux distros) go to this path.
-        upload_dir = os.path.join("/home/uploaded_files/")
-
-    # Get a list of all image files in the upload directory
-    file_list = [f for f in os.listdir(upload_dir) if os.path.isfile(os.path.join(upload_dir, f)) and f.lower().endswith(('.jpg', '.jpeg', '.png', '.tiff', '.gif', '.bmp', '.psd', '.raw', '.dng', '.cr2', '.nef', '.sr2'))]
+    upload_dir = get_upload_directory()
+    file_list = filter_image_files(upload_dir)
 
     if not file_list:
         return templates.TemplateResponse('no_files_found.html', context={'request': request}, status_code=200, media_type='text/html')
-    else:
-        # Pass the file list to the template
-        return templates.TemplateResponse(
-            'file_list_meta.html',
-            context={'request': request, 'file_list': file_list, 'stripname': stripname})
+
+    return templates.TemplateResponse(
+        'file_list_meta.html',
+        context={'request': request, 'file_list': file_list, 'stripname': stripname}
+    )
--- api/upload.py
+++ api/upload.py
@@ -2,31 +2,36 @@ import os
 import platform
 import secrets
 import shutil
-from typing import List
-from fastapi import APIRouter, Request, Form, UploadFile, File
-from fastapi.responses import FileResponse
-from fastapi.templating import Jinja2Templates
-from PIL import Image
 import subprocess
 import time
 import shlex
 import logging
+from typing import List, Tuple
+
+from fastapi import APIRouter, Request, Form, UploadFile, File
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+from PIL import Image
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates/")
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
-# Add a StreamHandler to log to console
-console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
+def configure_logging():
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
-# Function to create a folder for the session_id
-def create_session_folder(session_id):
+    # Add a StreamHandler to log to console
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
 
+
+configure_logging()
+
+
+def create_session_directory(session_id: str) -> Tuple[str, str]:
     current_dir = os.path.abspath(os.path.dirname(__file__))
     parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
     upload_dir = os.path.join(str(parent_dir), "uploaded_files", session_id)
@@ -38,32 +43,38 @@ def create_session_folder(session_id):
 
 
 @router.post('/api/upload')
-async def upload_files(request: Request, files: List[UploadFile] = File(..., max_size=100000000)): 
-
+async def upload_files(request: Request, files: List[UploadFile] = File(..., max_size=100000000)):
     # Get the session ID from the request cookies
     session_id = request.cookies.get('session_id')
-
     logging.info(f"Session ID retrieved from request cookies: {session_id}")
 
-    # Create a folder for the session_id
-    upload_dir, current_dir = create_session_folder(session_id)
+    # Create a directory for the session_id
+    upload_dir, _ = create_session_directory(session_id)
+    logging.info(f"Directory created for session ID {session_id} at {upload_dir}")
 
-    logging.info(f"Folder created for session ID {session_id} at {upload_dir}")
-
-    # Divide into batches, bitches.
     logging.info("Starting file upload")
 
-    this_dir = (os.getcwd())
-
-    for file in files:
-        contents = await file.read()
-        time.sleep(1)
-        logging.info(f"Received file {file.filename}")
-        with open(os.path.join(str(upload_dir), file.filename), "wb") as f:
-            f.write(contents)
+    # Safely change directories using a context manager
+    with change_directory(os.getcwd()):
+        for file in files:
+            contents = await file.read()
+            time.sleep(1)  # Simulate processing delay
+            logging.info(f"Received file {file.filename}")
+            with open(os.path.join(str(upload_dir), file.filename), "wb") as f:
+                f.write(contents)
 
     logging.info("File upload complete")
-    os.chdir(this_dir)
-    logging.info(f"Changed directory back to {this_dir}")
 
     return templates.TemplateResponse('upload.html', context={'request': request}, headers={'Cache-Control': 'no-cache'})
+
+
+class change_directory:
+    def __init__(self, path):
+        self.path = path
+        self.original_path = os.getcwd()
+
+    def __enter__(self):
+        os.chdir(self.path)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        os.chdir(self.original_path)
